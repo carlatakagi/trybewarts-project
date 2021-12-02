3fe10e4ebf7ff1962f8f5cb8648896e85edfc619
carlatakagi <carlatakagi@gmail.com>
2021-12-02 15:11:05 -0400

    tentativa requisito 3

---
+++ script.js
@@ -0,0 +1,15 @@
+const email = document.querySelector('#email').value;
+// const email2 = email.value;
+console.log('Olá, ', email);
+const senha = document.querySelector('#senha').value;
+console.log('Olá, ', senha);
+const senhaPadrao = parseInt('123456', 10);
+const botaoEntrar = document.querySelector('#botaoEntrar');
+function validarEmail() {
+  if (email === 'tryber@teste.com') {
+    window.alert('Olá, Tryber!');
+  } else {
+    window.alert('Email ou senha inválidos');
+  }
+}
+botaoEntrar.addEventListener('click', validarEmail);
