fc67436d12b46f2c2ebf9caab0a87f7d76405d3f
Kamila Hydalgo <kamilahydalgo@gmail.com>
2021-12-02 19:07:17 -0300

    Requisito 3 refatorado e 18 feito

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Requisito 3.
 const email = document.querySelector('#email');
 const senha = document.querySelector('#senha');
 const botaoEntrar = document.querySelector('#botaoEntrar');
@@ -6,15 +7,22 @@ function validarEmail(event) {
   if (email.value === 'tryber@teste.com' && senha.value === '123456') {
     window.alert('Olá, Tryber!');
   } else {
-    window.alert('Email ou senha inválidos');
+    window.alert('Email ou senha inválidos.');
   }
 }
 botaoEntrar.addEventListener('click', validarEmail);
 
+// Requisito 18.
+// Referência: https://developer.mozilla.org/en-US/docs/Web/API/HTMLSelectElement/disabled
 const enviarBotao = document.querySelector('#submit-btn');
 console.log(enviarBotao);
 const checkBox = document.querySelector('#agreement');
 console.log(checkBox);
 
-enviarBotao.addEventListener('click', () => {
-});
+checkBox.addEventListener('change', (event) => {
+  if (event.target.checked) {
+    enviarBotao.disabled = false;
+  } else {
+    enviarBotao.disabled = true;
+  }
+}, false);
