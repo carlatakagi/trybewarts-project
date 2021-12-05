fff067ff9b44ed866583c7a9e61bf2d4a35d8092
Kamila Hydalgo <kamilahydalgo@gmail.com>
2021-12-05 11:42:05 -0300

    Requisito 20 feito

--- script.js
+++ script.js
@@ -15,9 +15,7 @@ botaoEntrar.addEventListener('click', validarEmail);
 // Requisito 18.
 // Referência: https://developer.mozilla.org/en-US/docs/Web/API/HTMLSelectElement/disabled
 const enviarBotao = document.querySelector('#submit-btn');
-console.log(enviarBotao);
 const checkBox = document.querySelector('#agreement');
-console.log(checkBox);
 
 checkBox.addEventListener('change', (event) => {
   if (event.target.checked) {
@@ -31,15 +29,25 @@ checkBox.addEventListener('change', (event) => {
 // 1) Deve existir um contador com o ID "counter" ok fiz um
 // 2) O contador de caracteres deve ser atualizado conforme o conteúdo do textarea muda.
 // Referência: https://www.horadecodar.com.br/2020/12/09/contador-de-caracteres-em-uma-textarea-com-javascript/
-const textarea = document.querySelector('.textarea');
-const contador = document.querySelector('#counter');
-textarea.addEventListener('keypress', (e) => { // alteração para keypress - evento ocorre quando a tecla retorna à posição original no teclado (i.e. quando você solta a mesma).
-  const maximoCaracteres = 500;
-  const caracteresTextarea = textarea.length; // tirei o value, pois estava com erro e como nao pegamos o valor em si e somente o tamanho (caracteres) acredito que deveria ser só o lenght (nao deu erro com length kkk)
-  // const resultadoContador = maximoCaracteres - caracteresTextarea; - teriamos que colocar em outra funçao ou na mesma para mostrar na tela?
+const textArea = document.querySelector('#textarea');
 
-  // contador.display(e);
-  if (caracteresTextarea >= maximoCaracteres) {
-    e.preventDefault();
-  }
+textArea.addEventListener('keyup', () => {
+  const counter = document.querySelector('#counter');
+  const caracteresTextArea = textArea.value.length;
+  // console.log('Ola, ', caracteresTextArea);
+  counter.innerText = 500 - caracteresTextArea;
 });
+// 1-Coloquei a variável 'counter' dentro da função, pois o lint estava reclamando que ela não estava sendo chamada e deu certo.
+// 2-Sem o value na linha 36 o console.log so me retornava o 'Ola', sem o número de caracteres.
+// 3-Coloquei o 'counter.innerText' para somente retornar o texto e não a tag toda.
+// 4-Como já tínhamos estipulado o valor máximo de 500 caracteres na tag pela propriedade 'maxlength', não precisamos fazer esse prevant default.
+// 5-Usando o 'keypress', não sei pq quando apagamos algo na text area o contador não ia aumentando os caracteres e não passava no teste. Testei o 'keyup' e deu certo.
+
+// alteração para keypress - evento ocorre quando a tecla retorna à posição original no teclado (i.e. quando você solta a mesma).
+// const maximoCaracteres = 500;
+// const caracteresTextArea = textArea.length; // tirei o value, pois estava com erro e como nao pegamos o valor em si e somente o tamanho (caracteres) acredito que deveria ser só o lenght (nao deu erro com length kkk)
+// const resultadoContador = maximoCaracteres - caracteresTextarea; - teriamos que colocar em outra funçao ou na mesma para mostrar na tela?
+// contador.display(e);
+//   if (caracteresTextArea >= maximoCaracteres) {
+//     e.preventDefault();
+//   }
