25780ddd6e85f604d70ec36f6c7a6f9fdc5a01a0
Kamila Hydalgo <kamilahydalgo@gmail.com>
2021-12-05 12:01:03 -0300

    refatoraçao Requisito 3 e alteraçao de alguns nomes de variaveis

--- script.js
+++ script.js
@@ -2,32 +2,30 @@
 const email = document.querySelector('#email');
 const senha = document.querySelector('#senha');
 const botaoEntrar = document.querySelector('#botaoEntrar');
-function validarEmail(event) {
-  event.preventDefault();
+function logIn() {
+  // event.preventDefault(); Comentei essa linha para testar e realmente não é necessária para atender os requisitos pedidos.
   if (email.value === 'tryber@teste.com' && senha.value === '123456') {
-    window.alert('Olá, Tryber!');
+    alert('Olá, Tryber!');
   } else {
-    window.alert('Email ou senha inválidos.');
+    alert('Email ou senha inválidos.');
   }
 }
-botaoEntrar.addEventListener('click', validarEmail);
+botaoEntrar.addEventListener('click', logIn);
 
 // Requisito 18.
 // Referência: https://developer.mozilla.org/en-US/docs/Web/API/HTMLSelectElement/disabled
-const enviarBotao = document.querySelector('#submit-btn');
+const submitButton = document.querySelector('#submit-btn');
 const checkBox = document.querySelector('#agreement');
 
 checkBox.addEventListener('change', (event) => {
   if (event.target.checked) {
-    enviarBotao.disabled = false;
+    submitButton.disabled = false;
   } else {
-    enviarBotao.disabled = true;
+    submitButton.disabled = true;
   }
 }, false);
 
 // Requisito 20.
-// 1) Deve existir um contador com o ID "counter" ok fiz um
-// 2) O contador de caracteres deve ser atualizado conforme o conteúdo do textarea muda.
 // Referência: https://www.horadecodar.com.br/2020/12/09/contador-de-caracteres-em-uma-textarea-com-javascript/
 const textArea = document.querySelector('#textarea');
 
@@ -40,7 +38,7 @@ textArea.addEventListener('keyup', () => {
 // 1-Coloquei a variável 'counter' dentro da função, pois o lint estava reclamando que ela não estava sendo chamada e deu certo.
 // 2-Sem o value na linha 36 o console.log so me retornava o 'Ola', sem o número de caracteres.
 // 3-Coloquei o 'counter.innerText' para somente retornar o texto e não a tag toda.
-// 4-Como já tínhamos estipulado o valor máximo de 500 caracteres na tag pela propriedade 'maxlength', não precisamos fazer esse prevant default.
+// 4-Como já tínhamos estipulado o valor máximo de 500 caracteres na tag pela propriedade 'maxlength', não precisamos fazer esse prevant default. Mas o resto do raciocínio era esse mesmo que vc colocou.
 // 5-Usando o 'keypress', não sei pq quando apagamos algo na text area o contador não ia aumentando os caracteres e não passava no teste. Testei o 'keyup' e deu certo.
 
 // alteração para keypress - evento ocorre quando a tecla retorna à posição original no teclado (i.e. quando você solta a mesma).
